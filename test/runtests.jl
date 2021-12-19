562a401172251334aac93556b657dde5737c51f7
Edouard <edouard.oudet@imag.fr>
2021-12-19 22:34:55 +0100

    correct tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,8 @@ using MyExample
 using Test
 
 @testset "MyExample.jl" begin
-    @tesy myf(1, 1) == 5
-    @tesy myf(2, 1) == 7  
-    @tesy myf(2, 2) == 10  
+    @test my_f(1, 1) == 5
+    @test my_f(2, 1) == 7  
+    @test my_f(2, 2) == 10  
     # Write your tests here.
 end
